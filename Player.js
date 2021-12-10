ff31a7833a246adc88202d8fecba45c5cd4b8732
fujikijun <79777115+fujikijun@users.noreply.github.com>
2021-12-10 16:06:59 +0900

    Update Player.js

--- Player.js
+++ Player.js
@@ -4,6 +4,9 @@
 //
 //------------------------------------------------------------------------------------------
 
+let PLAYER_STATE_WAIT = 0;
+let PLAYER_STATE_MOVE = 1;
+
 class Player
 {
   constructor()
@@ -12,8 +15,10 @@ class Player
     this.y = 0;
     this.direction = 0;
     this.frame = 0;
+    this.state = PLAYER_STATE_WAIT;
+
     this.frameSpeed = 0.05;
-    this.speed = 20;
+    this.speed = 16;
     this.m_imgPlayer = [];
   }
 
@@ -27,6 +32,8 @@ class Player
     this.y = 159 * BASE_MAPTIP_SIZE;
     this.direction = 0;
     this.frame = 0;
+    this.state = PLAYER_STATE_WAIT;
+
     for ( let i=0; i<4; i++ )
     {
       this.m_imgPlayer[i] = [];
@@ -35,7 +42,7 @@ class Player
         this.m_imgPlayer[i][j] = loadImage( "data/player" +i+j+ ".png" );
       }
     }
-    
+
     this.init();
   }
 
@@ -54,35 +61,85 @@ class Player
 
   update()
   {
-    //if ( keyIsPressed && keyCode === RIGHT_ARROW )
-    if( keyRight )
+    switch( this.state )
     {
-      this.x += this.speed;
-      this.direction = 3;
-    }
-    //if ( keyIsPressed && keyCode === LEFT_ARROW )
-    if( keyLeft )
-    {
-      this.x -= this.speed;
-      this.direction = 1;
-    }
-    //if ( keyIsPressed && keyCode === DOWN_ARROW )
-    if( keyDown )
-    {
-      this.y += this.speed;
-      this.direction = 0;
-    }
-    //if ( keyIsPressed && keyCode === UP_ARROW )
-    if( keyUp )
-    {
-      this.y -= this.speed;
-      this.direction = 2;
-    }
-    
-    this.frame += this.frameSpeed;
-    if( this.frame >= 1.0 )
-    {
-      this.frame -= 1.0;
+      // ------------------------------------------------
+      // PLAYER_STATE_WAIT
+      // ------------------------------------------------
+
+    case PLAYER_STATE_WAIT:
+      {
+        //if ( keyIsPressed && keyCode === RIGHT_ARROW )
+        if ( keyRight )
+        {
+          this.x += this.speed;
+          this.direction = 3;
+          this.state = PLAYER_STATE_MOVE;
+        }
+        //if ( keyIsPressed && keyCode === LEFT_ARROW )
+        if ( keyLeft )
+        {
+          this.x -= this.speed;
+          this.direction = 1;
+          this.state = PLAYER_STATE_MOVE;
+        }
+        //if ( keyIsPressed && keyCode === DOWN_ARROW )
+        if ( keyDown )
+        {
+          this.y += this.speed;
+          this.direction = 0;
+          this.state = PLAYER_STATE_MOVE;
+        }
+        //if ( keyIsPressed && keyCode === UP_ARROW )
+        if ( keyUp )
+        {
+          this.y -= this.speed;
+          this.direction = 2;
+          this.state = PLAYER_STATE_MOVE;
+        }
+
+        this.frame += this.frameSpeed;
+        if ( this.frame >= 1.0 )
+        {
+          this.frame -= 1.0;
+        }
+      }
+      break;
+
+      // ------------------------------------------------
+      // PLAYER_STATE_MOVE
+      // ------------------------------------------------
+
+    case PLAYER_STATE_MOVE:
+      {
+        switch( this.direction )
+        {
+        case 0:  
+          this.y += this.speed;  
+          break;
+        case 1:  
+          this.x -= this.speed;  
+          break;
+        case 2:  
+          this.y -= this.speed;  
+          break;
+        case 3:  
+          this.x += this.speed;  
+          break;
+        }
+
+        if ( int(this.x)%BASE_MAPTIP_SIZE == 0 && int(this.y)%BASE_MAPTIP_SIZE == 0 )
+        {
+          this.state = PLAYER_STATE_WAIT;
+        }
+
+        this.frame += this.frameSpeed;
+        if ( this.frame >= 1.0 )
+        {
+          this.frame -= 1.0;
+        }
+      }
+      break;
     }
   }
 
